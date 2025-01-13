656a3fb984e44daa592fbff8278c4f04604278e1
koron <koronski.marek@gmail.com>
2025-01-13 17:32:26 +0100

    data

--- src/app/state/monitor/monitor.effects.ts
+++ src/app/state/monitor/monitor.effects.ts
@@ -1,26 +1,41 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect } from '@ngrx/effects';
-import { merge, of } from 'rxjs';
+import { filter, merge, of, startWith, switchMap } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import {
   loadMonitorItemsSuccess,
   loadMonitorItemsFailure,
 } from './monitor.actions';
 import { DirectMonitorService } from '../../services/direct-monitor.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable()
 export class MonitorEffects {
+  auth = inject(AngularFireAuth);
+  authState = this.auth.authState.pipe(startWith(undefined));
+
   monitorItemService = inject(DirectMonitorService);
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private router: Router
+  ) {}
 
   loadMonitorItems$ = createEffect(() =>
-    merge(
-      this.monitorItemService.getAllSnapshot(),
-      this.monitorItemService.getAllChanged()
-    ).pipe(
-      map((monitorItems) => loadMonitorItemsSuccess({ monitorItems })),
-      catchError((error) =>
-        of(loadMonitorItemsFailure({ error: error.message }))
+    this.authState.pipe(
+      switchMap(() =>
+        merge(
+          this.monitorItemService.getAllSnapshot(),
+          this.monitorItemService.getAllChanged()
+        ).pipe(
+          map((monitorItems) => loadMonitorItemsSuccess({ monitorItems })),
+          catchError((error) => {
+            if (error.code === 'permission-denied') {
+              this.router.navigate(['/login']);
+            }
+            return of(loadMonitorItemsFailure({ error: error.message }));
+          })
+        )
       )
     )
   );
