15962f07bc9645a5e5b3f220b43c2c36b99ae20f
koron <koronski.marek@gmail.com>
2025-01-22 15:55:37 +0100

    automation

---
+++ src/app/firebase.alpha.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBtfJcT23phGL9Xk-PXuPT5Vc1emJbbc44',
+  authDomain: 'solar-golan.firebaseapp.com',
+  projectId: 'solar-golan',
+  storageBucket: 'solar-golan.appspot.com',
+  messagingSenderId: '870051366241',
+  appId: '1:870051366241:web:d838a50ea33ac17d86f120',
+};
